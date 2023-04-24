97330d01965cc554d7133158fe2e11cd12186f12
Geeta Hangma <limboogeetahangma@gmail.com>
2023-04-24 15:30:16 +0530

    first push

---
+++ tuples.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+m_list=[1,23,43,324]
+
+
+# In[14]:
+
+
+m_tup=(1,2,3,5)
+
+
+# In[3]:
+
+
+type(m_list)
+
+
+# In[4]:
+
+
+type (m_list)
+
+
+# In[5]:
+
+
+type(m_list)
+
+
+# In[6]:
+
+
+type(m_list)
+
+
+# In[11]:
+
+
+type(m_list)
+
+
+# In[9]:
+
+
+type(m_tup) 
+
+
+# In[15]:
+
+
+len(m_tup)
+
+
+# In[16]:
+
+
+m_tup[-1]
+
+
+# In[21]:
+
+
+t=("a","a","b","c","d",)
+   
+
+
+# In[ ]:
+
+
+
+
+
+# In[23]:
+
+
+t.count('a')
+
+
+# In[24]:
+
+
+t.index('a')
+
+
+# In[25]:
+
+
+myset= set()
+
+
+# In[26]:
+
+
+myset
+
+
+# In[27]:
+
+
+myset.add(1)
+
+
+# In[28]:
+
+
+myset
+
+
+# In[29]:
+
+
+myset.add(2)
+
+
+# In[30]:
+
+
+myset
+
+
+# In[34]:
+
+
+mylist=[1,2,3,4,5,3,2,1,4,56,3,2,3,]
+
+
+# In[36]:
+
+
+set(mylist)
+
+
+# In[37]:
+
+
+True
+
+
+# In[38]:
+
+
+False
+
+
+# In[39]:
+
+
+1<2
+
+
+# In[40]:
+
+
+1>2
+
+
+# In[41]:
+
+
+1==1
+
+
+# In[42]:
+
+
+b=None
+
+
+# In[43]:
+
+
+type(b)
+
+
+# In[ ]:
+
+
+
+
